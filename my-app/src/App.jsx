68224e2d6d5f1c60748d8ca7e7c6430855575e7f
shahedfadialjamal <shahedfadi99@gmail.com>
2025-12-03 20:15:54 +0300

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
-import Home from "./pages/Home.jsx";
+import Home from "./pages/home.jsx";
 import Login from "./pages/login.jsx";
 import Register from "./pages/register.jsx";
 import About from "./pages/about.jsx";
